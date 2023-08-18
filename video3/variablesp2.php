7681a249dc0c7b4d53e02eabd1b4b874eed35867
GustavoFranco <gustavofrancoxd@outlook.com>
2023-08-17 18:03:41 -0700

    segundo video del curso, mas sobre variables

---
+++ video3/variablesp2.php
@@ -0,0 +1,35 @@
+<?php
+//  esto es un comentario
+
+/*
+    esto es un comentario
+    en bloque de codigo
+ */
+
+
+//caracter
+$caracter = 'a';
+$numero = 4;
+$cadena = "esto es una cadena";
+$flotante =3.14;
+$booleano = true;
+$arregloNumerico = [1,2,3,4];
+$arregloDeCadenas = ["Gustavo", "Joaquin", "Jeyson"];
+$carros = array("toyota", "ford", "ferrari");
+$edad = array(18,53, 43, 69);
+$nada= null;
+
+echo $cadena . "<br/>";
+echo $flotante . "<br/>";
+echo $booleano . "<br/>";
+echo $nada . "<br/>";
+
+print_r($arregloDeCadenas);
+echo "<br/>";
+print_r($arregloNumerico);
+echo "<br/>";
+print_r($carros);
+
+
+
+?>
